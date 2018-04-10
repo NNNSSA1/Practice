0db709a60b1598ef2331f0bbd49fb4345e59b225
wade <352912788@qq.com>
2018-04-10 15:37:06 +0800

    last

--- demo.sh
+++ demo.sh
@@ -1,9 +1,16 @@
-mkdir $1
-cd $1
-mkdir css js
-echo "<!DOCTYPE>
+if [ -d $1 ]; then
+	echo "存在了"
+	exit 1
+else
+     mkdir $1
+     cd $1
+     mkdir css js    
+	echo "<!DOCTYPE>
         <title>Hello</title>
         <h1>Hi</h1>"> index.html
-echo "h1{color: red;}"> css/style.css
-echo "var string = "Hello World"
+	echo "h1{color: red;}"> css/style.css
+	echo "var string = "Hello World"
         alert(string)"> js/main.js
+	echo "成功"
+	exit 0
+fi
